aa93c757651c5f76dc17090d1be663adb2527f45
Hayate887 <sakusochisakusaku@gmail.com>
2024-07-26 15:33:41 +0900

    change my research

--- src/pages/selfintro.tsx
+++ src/pages/selfintro.tsx
@@ -1,5 +1,10 @@
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
   Box, Button, Center, Heading,
   Menu, MenuButton,
   MenuItem, MenuList,
@@ -14,7 +19,7 @@ export default function English() {
         <MenuButton as={Button} rightIcon={<ChevronDownIcon />} position="absolute"
             top='5'
             right="5" >
-              ログイン画面に戻る
+              メニュー
             </MenuButton>
             <MenuList>
             <Link href='set_selfintro'  target='_blank'>
@@ -31,9 +36,9 @@ export default function English() {
           </Menu>
       </Box>
 
-  <Box p={4}>
+  <Box p={5}>
     <Center>
-      <Heading as='h1'>hayateの自己紹介ページ</Heading>
+      <Heading as='h1' color='red'>hayateの自己紹介ページ</Heading>
     </Center>
 
   </Box>
@@ -52,7 +57,72 @@ export default function English() {
       </TabPanel>
 
       <TabPanel>
-        <Heading as='h1'>研究テーマについて</Heading>
+        <Heading as='h2' p={3} mb={5}>
+          <Center>
+            2段階ファインチューニングによる個性抽出手法の提案
+            </Center>
+            </Heading>
+        <Accordion defaultIndex={[-1]} allowMultiple>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as='span' flex='1' textAlign='left'>
+                  目的
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                少ないデータでも精度の高い個性の抽出を行えるようにしたい
+                </AccordionPanel>
+                </AccordionItem>
+
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box as='span' flex='1' textAlign='left'>
+                        方法
+                        </Box>
+                        <AccordionIcon />
+                        </AccordionButton>
+                        </h2>
+                        <AccordionPanel pb={4}>
+                          <p>1. 青空文庫で公開されてい著作権が切れた全作家の作品を一つ選び、事前学習済みモデルでファインチューニングを行う</p>
+                          <p>2. 個性を抽出したい作家の全作品（パブリックドメイン）を、事前学習済みモデルと1で作成したモデルでそれぞれファインチューニングを行う</p>
+                          <p>3. コサイン類似度やBLUEスコア等で性能評価を行う</p>
+                </AccordionPanel>
+                </AccordionItem>
+
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box as='span' flex='1' textAlign='left'>
+                        現在の進捗
+                        </Box>
+                        <AccordionIcon />
+                        </AccordionButton>
+                        </h2>
+                        <AccordionPanel pb={4}>
+                         ・データセット構築の途中
+                         <p>・夏休み終了時までには少なくとも一人データセットの構築を行い、性能評価まで行いたい</p>
+                          </AccordionPanel>
+                          </AccordionItem>
+
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box as='span' flex='1' textAlign='left'>
+                        最終目標
+                        </Box>
+                        <AccordionIcon />
+                        </AccordionButton>
+                        </h2>
+                        <AccordionPanel pb={4}>
+                         いい結果が出たらこのモデルをチャットbotにして、VR空間で使用できるようにしたい
+                          </AccordionPanel>
+                          </AccordionItem>
+                </Accordion>
+                
       </TabPanel>
 
     </TabPanels>
